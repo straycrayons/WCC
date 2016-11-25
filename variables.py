224c2fd2cdf4ab92edf5fea8d85950019b6f9ce7
straycrayons <Main@Mains-MacBook-Pro.local>
2016-11-25 13:58:05 -0500

    madlib assignment

---
+++ variables.py
@@ -0,0 +1,6 @@
+#name = raw_input('What is your name? ')
+#print('Hi ' + name)
+
+age = raw_input('How old are you?')
+dog_years = int(age) * 7
+print('You are ' + str(dog_years) + 'old in dog years.')
