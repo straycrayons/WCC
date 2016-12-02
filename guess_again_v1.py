479a08ba31c8b127e458deb15844ee07847e0b2d
straycrayons <Main@Mains-MacBook-Pro.local>
2016-12-02 11:38:48 -0500

    Other tasks in this weeks lessons

---
+++ guess_again_v1.py
@@ -0,0 +1,6 @@
+for guess_count in range(0,4):
+
+    if guess_count == 3:
+        break
+
+    print guess_count
---
+++ loops.py
@@ -0,0 +1,4 @@
+def print_vowels():
+    vowels = 'aeiou'
+    for letter in vowels:
+        print letter
